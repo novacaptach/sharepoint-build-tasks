c753816b03508f4cd98ca1d8f5c6fef49736b397
Pascal Berger <pascal.berger@ioz.ch>
2015-12-12 11:25:55 +0100

    Update extension packaging

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var del = require('del');
 var shell = require('shelljs')
 var pkgm = require('./package');
 var ts = require('gulp-typescript');
-var tfx = require('./node_modules/tfx-cli/_build/app/exec/extension/create');
+var tfx_extension_create = require('tfx-cli/_build/app/exec/extension/create');
 
 var _buildRoot = path.join(__dirname, '_build');
 var _tasksRoot = path.join(_buildRoot, 'Tasks');
@@ -29,19 +29,20 @@ gulp.task('compile', function (cb) {
 	return tsResult.js.pipe(gulp.dest(path.join(__dirname, 'Tasks')));
 });
 
-gulp.task('package', ['build'], function() {
+gulp.task('package', ['build'], function(cb) {
 	shell.mkdir('-p', _extensionsRoot);
-	tfx.createExtension(
-		{
-			root: './',
-			manifestGlobs: ['vss-extension.json'],
-			overrides: {},
-			bypassValidation: false
-		}, 
-		{
-			outputPath: _extensionsRoot,
-			locRoot: null
-		});			
+	var common = require('tfx-cli/_build/app/lib/common');
+    var command = tfx_extension_create.getCommand([
+      '--output-path', _extensionsRoot
+    ]);
+    common.EXEC_PATH = ['extension', 'create'];
+    command.exec().then(function() {
+      console.log('Sucessfully created extension package');
+      cb();
+    }, function(reason) {
+      console.error('Unable to create extension package because ', reason);
+      cb(reason);
+    });
 });
 
 gulp.task('build', ['clean', 'compile'], function () {
